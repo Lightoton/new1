5c4faaa9324bc24437d69df82ab345fb2ad477ed
Viktor <lightoton@gmail.com>
2023-09-27 20:42:25 +0200

    Lesson2 - 11

---
+++ src/main/java/de/telran/lesson3/lesson3.java
@@ -0,0 +1,17 @@
+package de.telran.lesson3;
+
+public class lesson3 {
+    static char classchar;
+    public static void main(String[] args) {
+        int myInt =13;
+        System.out.println(myInt);
+        System.out.println(classchar);
+
+        printValue();
+    }
+
+    static void printValue(){
+        int myInt = 18;
+        System.out.println(myInt);
+    }
+}
